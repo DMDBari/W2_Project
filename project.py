9b768093c262a2f936a60e986f12d96cc2f5b929
Dillon Dains <dilloncsbb@gmail.com>
2024-03-03 18:22:26 -0600

    finished adding functions

--- project.py
+++ project.py
@@ -7,7 +7,9 @@ def shopping_cart():
     item_total = 0.0
     total = 0.0
     action = ''
-    print('Thank you for using our Shopping Cart')
+    clear = ''
+    change = True
+    print('Thank you for using our Shopping Cart App')
     while action.title() != 'Quit':
         action = input('What would you like to do? (Add/Remove/Display/Clear/Quit)')
         if action.title() == 'Add':
@@ -15,8 +17,18 @@ def shopping_cart():
             cost = input(f"What is the price of {item}?")
             quantity = input('How many would you like to add?')
             unit = {'item': item, 'cost': cost, 'quantity':quantity}
-            output.append(unit)
-            print(f"{item} has been added to the cart.")
+            for dictionary in output:
+                if unit['item'] == dictionary['item']:
+                    dictionary['cost'] = cost
+                    dictionary['quantity'] = quantity
+                    change = False
+                    break
+            if change:
+                output.append(unit)
+                print(f"{item} has been added to your cart")
+            else:
+                change = True
+                print(f"{item} has been updated")
         elif action.title() == 'Remove':
             item = input('What item would you like to remove?').capitalize()
             for i in range(len(output)):
@@ -31,21 +43,28 @@ def shopping_cart():
                 removed = True
         elif action.title() == 'Display':
             print('Your current cart includes:\n')
-            for dict in output:
-                item_total = int(dict['quantity']) * float(dict['cost'])
+            for dictionary in output:
+                item_total = int(dictionary['quantity']) * float(dictionary['cost'])
                 total += item_total
                 print('-'*15)
-                print(f'Item: {dict['item']}, quantity: {dict['quantity']}, total: ${item_total} ')
+                print(f'Item: {dictionary['item']}, quantity: {dictionary['quantity']}, total: ${item_total} ')
                 print('-'*15)
             print(f'Your current total is ${round(total, 2)}')
             total = 0.0
-        # elif action.title() == 'Clear':
+        elif action.title() == 'Clear':
+            clear = input("Are you sure?  This will permenantly delete all items in cart. (Yes/No)")
+            if clear.title() == 'Yes':
+                output = []
     else:
-        for items, costs, totals in output.items():
+        print('Your current cart includes:\n')
+        for dictionary in output:
+            item_total = int(dictionary['quantity']) * float(dictionary['cost'])
+            total += item_total
             print('-'*15)
-            print(f"")
+            print(f'Item: {dictionary['item']}, quantity: {dictionary['quantity']}, total: ${item_total} ')
             print('-'*15)
-        print('')
+        print(f'Your current total is ${round(total, 2)}')
+        print('Thank you for using our Shopping Cart App')
         return
     
 shopping_cart()
